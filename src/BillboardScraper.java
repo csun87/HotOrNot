a20cb0044d75d412ea65b1111381d14b64119076
csun87 <christiansun7@gmail.com>
2022-04-30 11:08:43 -0400

    Finished Billboard Hot 100 scraper

--- src/BillboardScraper.java
+++ src/BillboardScraper.java
@@ -2,11 +2,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.util.*;
 
 import java.io.IOException;
 
 public class BillboardScraper {
-    private String URL = "https://www.billboard.com/charts/hot-100/";
+    private final String URL = "https://www.billboard.com/charts/hot-100/";
     private Document doc;
 
     public BillboardScraper() {
@@ -15,5 +16,25 @@ public class BillboardScraper {
         } catch (IOException e) {
             System.out.println("Could not connect to the Billboard Hot 100 base URL!");
         }
+    }
+
+    public List<List<String>> getRanking() {
+        List<List<String>> ans = new ArrayList<>();
+        Elements songs = this.doc.select(".o-chart-results-list-row-container");
+        for (Element song : songs) {
+            List<String> pair = new ArrayList<>(2);
+            Element title = song.getElementById("title-of-a-story");
+            if (title == null) {
+                continue;
+            }
+            Element artist = title.nextElementSibling();
+            if (artist == null) {
+                continue;
+            }
+            pair.add(title.text());
+            pair.add(artist.text());
+            ans.add(pair);
+        }
+        return ans;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        BillboardScraper scraper = new BillboardScraper();
+        List<List<String>> ranking = scraper.getRanking();
+        int i = 1;
+        for (List<String> l : ranking) {
+            System.out.println(i + ": " + l.get(0) + ", by: " + l.get(1));
+            ++i;
+        }
     }
 }
